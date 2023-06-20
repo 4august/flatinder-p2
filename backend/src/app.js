8ce5d6f35dfaae5bcaff087bd0a641159de4ee9c
Emanuel <emanuel.ibanhes.contato@gmail.com>
2023-06-20 00:12:23 -0400

    pre config, para ter um estado init do proj back

---
+++ backend/src/app.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+// instância do express
+const app = express()
+const router = express.Router();
+
+// conecta no banco
+mongoose.connect('mongodb+srv://emanuelibanhescontato:rJsOj64zUNXByQNG@cluster0.nzrr5rk.mongodb.net/')
+
+// carrega as rotas
+const index = require('./routes/index');
+const userRoute = require('./routes/user-route');
+
+app.use(bodyParser.json()); //converte para json
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', index);
+app.use('/cadastro-usuario', userRoute)
+app.use('/atualiza', userRoute)
+app.use('/deleta', userRoute)
+
+module.exports = app;
