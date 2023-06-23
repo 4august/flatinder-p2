d69b5d671964c147d6803c7063b641af52a653ef
4august <42296932+4august@users.noreply.github.com>
2023-06-22 22:42:48 -0400

    deleting old backend to avoid conflict

--- backend/src/collections/Interests.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const formInterestsSchema = new mongoose.Schema({
-    interests: "String"
-
-});
-
-const formInterests = mongoose.model('Interests', formInterestsSchema);
-module.exports = formInterests;
